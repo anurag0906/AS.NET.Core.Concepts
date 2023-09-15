850a6a38c1cd14619fcf4065c1de374a6fb59218
Anurag <anurag.shagun@evicore.com>
2023-09-14 22:19:03 -0500

    Add project files.

---
+++ AS.NET.Core.Routing.FeatureSwitching/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using AS.NET.Core.Routing.FeatureSwitching.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AS.NET.Core.Routing.FeatureSwitching.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[Route("mycontact", Name ="contactroute")]
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		[Route("Email")]
+		public IActionResult Email()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ AS.NET.Core.Routing.FeatureSwitching/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AS.NET.Core.Routing.FeatureSwitching.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
